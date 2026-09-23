4b74ffefb5043fa70e74544aaaa715328bbda5d1
Artem Kozin <akozin@car10s1.42barcelona.com>
2026-09-23 16:24:42 +0200

    let's continue

--- mybci.py
+++ mybci.py
@@ -7,9 +7,23 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-i", "--input-list", nargs="+", type=Path, required=True,
-        help="EDF files to read. Corresponding events are assumed to be named"
-             " as <file>.event"
+        "-i", "--input-dir", type=Path, required=True,
+        help="Directory of input .edf files. Must be of structure:\n"
+             "<dir>\n"
+             "    |-Patient 1 subdir\n"
+             "    |                |-...R01.edf\n"
+             "    |                |-...R02.edf\n"
+             "    |                |....\n"
+             "    |                |-...R<last-number>.edf\n"
+             "    |-Patient 2 subdir\n"
+             "    |...\n"
+             "IMPORTANT: This program assumes the following order of "
+             "recordings - R01 is baseline with eyes open, R02 is baseline "
+             "with eyes closed, while R03..R06 are eyes open/closed alterna"
+             "ting with the first two being left/right hand movement (T1/T2) "
+             "and the last two being hands/feet movement (T1/T2), repeated "
+             "afterwards (in the original dataset repeated 2 more times "
+             "until R14)"
     )
     args = parser.parse_args()
 
@@ -17,8 +31,8 @@ def parse_args():
 
 def main():
     args = parse_args()
-    raw0 = mne.io.read_raw_edf(args.input_list[0])
-    print(raw0)
+    print(list(args.input_dir.iterdir()))
+
 
 if (__name__=="__main__"):
     main()
